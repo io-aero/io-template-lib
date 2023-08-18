f90fd983552e0c6ac12b3631135595f381b0094b
Walter Weinmann <walter.weinmann@gmail.com>
2023-08-19 01:31:03 +0200

    Subpackages etc.

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2022 IO-Aero. All rights reserved. Use of this
 # source code is governed by the IO-Aero License, that can
 # be found in the LICENSE.md file.
-
 """Module iotemplatelib: Entry Point Functionality.
 
 This is the entry point to the library IO-TEMPLATE-LIB.
@@ -34,14 +33,9 @@ def main(argv: list[str]) -> None:
 
     io_glob.logger.debug(io_glob.LOGGER_START)
     io_glob.logger.debug("param argv=%s", argv)
-
-    io_glob.logger.info("Start launcher.py")
-
-    print("Start launcher.py")
-
     locale.setlocale(locale.LC_ALL, _LOCALE)
 
-    print("End   launcher.py")
+    io_glob.logger.info("Start launcher.py")
 
     io_glob.logger.info("End   launcher.py")
     io_glob.logger.debug(io_glob.LOGGER_END)
